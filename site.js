40a50a7b8c1e13a4cf1902cd80acbad58a6746f7
nzazula <nzazula@umich.edu>
2022-06-09 18:17:20 -0400

    some changes

--- site.js
+++ site.js
@@ -5,6 +5,8 @@ function myFunction() {
     if (x.style.display === "none" || y.style.display === "none" ) {
       x.style.display = "flex";
       y.style.display = "block";
+        hint.style.opacity = 1;
+        hint.style.transform = 'scale(1)';
     } else {
       x.style.display = "none";
       y.style.display = "none";
