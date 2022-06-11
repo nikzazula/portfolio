36f2b344a6d759e65e24d9d5c5156fdcc6ae3c3d
nzazula <nzazula@umich.edu>
2022-06-11 00:34:32 -0400

    adding resume

--- site.js
+++ site.js
@@ -1,6 +1,22 @@
+//dev pop up
 function myFunction() {
-    var x = document.getElementById("web-testing-div");
-    var y = document.getElementById("web-testing");
+    var x = document.getElementById("dev-pop-up-div");
+    var y = document.getElementById("dev-pop-up");
+
+    if (x.style.display === "none" || y.style.display === "none" ) {
+      x.style.display = "flex";
+      y.style.display = "block";
+        hint.style.opacity = 1;
+        hint.style.transform = 'scale(1)';
+    } else {
+      x.style.display = "none";
+      y.style.display = "none";
+    }
+  }
+  //resume popup
+  function popResume() {
+    var x = document.getElementById("resume-pop-up-div");
+    var y = document.getElementById("resume-pop-up");
 
     if (x.style.display === "none" || y.style.display === "none" ) {
       x.style.display = "flex";
