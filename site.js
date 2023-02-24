a02cff219faffdb04d4a0fca073ce1395a0b7f4c
nzazula <nzazula@umich.edu>
2023-02-24 00:42:13 -0500

    Update to product research

--- site.js
+++ site.js
@@ -1,10 +1,22 @@
 
-//dev pop up
-function myFunction() {
-  var x = document.getElementById("dev-pop-up-div");
-  var y = document.getElementById("dev-pop-up");
+// POP UPS ////////////////////////////////////////////
 
-  if (x.style.display === "none" || y.style.display === "none" ) {
+function popItem(name1){
+  var x = document.getElementById(name1);
+  if (x.style.display === "none" || x.style.display == '' ) {
+    x.style.display = "flex";
+    hint.style.opacity = 1;
+    hint.style.transform = 'scale(1)';
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// POP-PDF FUNCTION
+function popPDF(name1, name2){
+  var x = document.getElementById(name1);
+  var y = document.getElementById(name2);
+  if (x.style.display === "none" || x.style.display == '' || y.style.display === "none" || y.style.display == '') {
     x.style.display = "flex";
     y.style.display = "block";
       hint.style.opacity = 1;
@@ -14,22 +26,35 @@ function myFunction() {
     y.style.display = "none";
   }
 }
-//resume popup
+
+// WORK POP-UPS
+// DEVELOPMENT
+
+// RESEARCH
+function popResearch(){
+  popItem("research-items")
+}
+// MARKETING
+// FASHION NOVA USABILITY TESTING
+function popFS() {
+  popPDF("fs-pop-up-div", "fs-pop-up");
+}
+// WA NEEDS ASSESSMENT
+function popWA() {
+  popPDF("wa-pop-up-div", "wa-pop-up");
+}
+// SURVEY DESIGN
+function popSUR() {
+  popPDF("sur-pop-up-div", "sur-pop-up");
+}
+// RESUME
 function popResume() {
-  var x = document.getElementById("resume-pop-up-div");
-  var y = document.getElementById("resume-pop-up");
-
-  if (x.style.display === "none" || y.style.display === "none" ) {
-    x.style.display = "flex";
-    y.style.display = "block";
-      hint.style.opacity = 1;
-      hint.style.transform = 'scale(1)';
-  } else {
-    x.style.display = "none";
-    y.style.display = "none";
-  }
+  popPDF("resume-pop-up-div", "resume-pop-up");
 }
 
+
+
+// GALLERY INDICATORS
 const list = document.querySelector(".list");
 const items = Array.from(document.querySelectorAll(".item"));
 const indicators = Array.from(document.querySelectorAll(".indicator"));
